61339bb074a178a894eba812ae4e3d8f60cc3625
nawarani <nawarani47@gmail.com>
2019-09-12 10:55:21 -0400

    new_blog_post

---
+++ pelicanconf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Anika Nawar'
+SITENAME = 'blog'
+SITEURL = 'https://nawarani.github.io/blog'
+PATH = 'content'
+STATIC_PATHS = ['images']
+TIMEZONE = 'America/Toronto'
+DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
